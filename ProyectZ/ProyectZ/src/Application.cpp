efd1616a4d3dde6db9614ea912b804a44225eec1
Francisco <52508076+PelaezFrancisco@users.noreply.github.com>
2020-11-03 00:03:56 -0500

    Ventana OpenGl

---
+++ ProyectZ/ProyectZ/src/Application.cpp
@@ -0,0 +1,36 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+int main()
+{
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glfwInit();
+
+    //std::cout << glGetString(GL_VERSION) << std::endl;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    //glViewport(0, 0, 800, 600);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "ProyectZ", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    return 0;
+}
